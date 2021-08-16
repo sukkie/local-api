d1639e149ef462fdb10759faa2faaaa35621e02e
sukkie <sukkie512@yahoo.co.jp>
2021-08-17 00:09:18 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const PORT = 8080;
+const {posts} = require('./data')
+
+app.use(cors());
+app.get('/api/posts', (req, res) => {
+    res.json(posts);
+});
+
+app.listen(PORT, () => console.log(`Server running on ${PORT}`))
